3af27b0a5064bc5cdf03a332ce619d0a705427f0
Hussam Elnashar <hussam.nashar7@gmail.com>
2025-06-09 16:42:47 +0200

    Likelihood ratio test

--- lrt.py
+++ lrt.py
@@ -15,6 +15,9 @@ stat_2_comp= -2*(llf_null_2_comp-llf_alt_2_comp)
 
 diff_df=1
 
-print("p-value, both components", format(chi2.sf(stat_2_comp,diff_df),'.3f'))
+print("stat, short-term component", format(stat_short,'.3f'))
 print("p-value, short-term component", format(chi2.sf(stat_short,diff_df),'.3f'))
+print("stat, long-term component", format(stat_long,'.3f'))
 print("p-value, long-term component", format(chi2.sf(stat_long,diff_df),'.3f'))
+print("stat, both components", format(stat_2_comp,'.3f'))
+print("p-value, both components", format(chi2.sf(stat_2_comp,diff_df),'.3f'))
