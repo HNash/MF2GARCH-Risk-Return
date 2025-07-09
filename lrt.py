b2d629608880f6c3a888d914656d9df795e39278
Hussam Elnashar <hussam.nashar7@gmail.com>
2025-07-10 00:26:18 +0200

    Paper

--- lrt.py
+++ lrt.py
@@ -1,20 +1,20 @@
 import numpy as np
 from scipy.stats import chi2
 
-llf_null_short=-18567.209
-llf_alt_short=-18562.245
+llf_null_short=-18567.654
+llf_alt_short=-18560.751
 stat_short= -2*(llf_null_short-llf_alt_short)
 
-llf_null_long=-18558.337
-llf_alt_long=-18557.658
+llf_null_long=-18558.341
+llf_alt_long=-18558.324
 stat_long= -2*(llf_null_long-llf_alt_long)
 
-llf_null_2_comp=-18557.874
-llf_alt_2_comp=-18557.379
+llf_null_2_comp=-18558.035
+llf_alt_2_comp=-18557.764
 stat_2_comp= -2*(llf_null_2_comp-llf_alt_2_comp)
 
-llf_null_vol=-18562.833
-llf_alt_vol=-18560.408
+llf_null_vol=-18563.335
+llf_alt_vol=-18559.744
 stat_vol= -2*(llf_null_vol-llf_alt_vol)
 
 diff_df_crises=2
